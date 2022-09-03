e04f98f26ce7dd19839ccbc25af16b8d1eca4ac3
bbOracul <gilyazovRislan@gmail.com>
2022-09-03 17:28:16 +0500

    tests added

--- bit_components/like_picker/like_picker.spec.tsx
+++ bit_components/like_picker/like_picker.spec.tsx
@@ -1,10 +1,29 @@
 import React from "react";
-import { Renderer } from "react-dom";
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import { BasicLikePicker } from "./like_picker.composition";
+import LikePicker from "./like_picker";
 
-it("should render with the correct text", () => {
-  // render(<BasicLikePicker />);
-  // const rendered = screen.getByText("bad");
-  expect(true).toBeTruthy();
+test("number of filled stars", () => {
+  render(<BasicLikePicker />);
+  const element = screen.getAllByText("★");
+  expect(element.length).toBe(3);
+});
+
+test("number of empty stars", () => {
+  render(<BasicLikePicker />);
+  const elements = screen.getAllByText("☆");
+  expect(elements.length).toBe(2);
+});
+
+test("click test", () => {
+  const handleChange = jest.fn();
+  render(
+    <div className="App">
+      <LikePicker max={5} value={1} onChange={handleChange} />
+    </div>
+  );
+  let elements = screen.getAllByText("☆");
+  expect(elements.length).toBe(4);
+  fireEvent.click(elements[2]);
+  expect(handleChange).toBeCalledTimes(1);
 });
