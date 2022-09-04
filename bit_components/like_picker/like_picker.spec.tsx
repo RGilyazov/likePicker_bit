18338201cf338bf30cd34f7f070714a6193d6e6c
bbOracul <gilyazovRislan@gmail.com>
2022-09-04 09:37:53 +0500

    more tests added

--- bit_components/like_picker/like_picker.spec.tsx
+++ bit_components/like_picker/like_picker.spec.tsx
@@ -15,15 +15,36 @@ test("number of empty stars", () => {
   expect(elements.length).toBe(2);
 });
 
-test("click test", () => {
+test("click handleChange", () => {
   const handleChange = jest.fn();
   render(
     <div className="App">
       <LikePicker max={5} value={1} onChange={handleChange} />
     </div>
   );
-  let elements = screen.getAllByText("☆");
+  const elements = screen.getAllByText("☆");
   expect(elements.length).toBe(4);
   fireEvent.click(elements[2]);
   expect(handleChange).toBeCalledTimes(1);
+});
+
+test("click change state", async () => {
+  render(<BasicLikePicker />);
+  const elements = screen.getAllByText("☆");
+  expect(elements.length).toBe(2);
+  fireEvent.click(elements[0]);
+  expect(screen.getAllByText("☆").length).toBe(1);
+});
+
+test("hover effect", () => {
+  render(
+    <div className="App">
+      <LikePicker max={5} value={1} color="red" activeColor="green" />
+    </div>
+  );
+  let elements = screen.getAllByText("☆");
+  expect(elements[0]).toHaveStyle("color: red");
+  fireEvent.mouseOver(elements[0]);
+  elements = screen.getAllByText("☆");
+  expect(elements[0]).toHaveStyle("color: green");
 });
